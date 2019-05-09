cfbb5e4720617d225ef458e8c1038c6d0b471557
Yenuoyi <19689405@qq.com>
2019-05-09 14:59:03 +0800

    完善公共工程

---
+++ common-center/common-core/src/main/java/com/yb/common/center/redis/cluster/AppConfig.java
@@ -0,0 +1,28 @@
+package com.yb.common.center.redis.cluster;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+
+/**
+ * @author yebing
+ */
+@Configuration
+public class AppConfig {
+
+    /**
+     * Type safe representation of application.properties
+     */
+    @Autowired
+    ClusterConfigurationProperties clusterProperties;
+
+    public @Bean
+    RedisConnectionFactory connectionFactory() {
+
+        return new JedisConnectionFactory(
+                new RedisClusterConfiguration(clusterProperties.getNodes()));
+    }
+}
---
+++ common-center/common-core/src/main/java/com/yb/common/center/redis/cluster/ClusterConfigurationProperties.java
@@ -0,0 +1,30 @@
+package com.yb.common.center.redis.cluster;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author yebing
+ */
+@Component
+@ConfigurationProperties(prefix = "spring.redis.cluster")
+public class ClusterConfigurationProperties {
+
+    List<String> nodes;
+
+    /**
+     * Get initial collection of known cluster nodes in format {@code host:port}.
+     *
+     * @return
+     */
+    public List<String> getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(List<String> nodes) {
+        this.nodes = nodes;
+    }
+}
+
